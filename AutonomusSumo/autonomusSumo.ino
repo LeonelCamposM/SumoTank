eb87d40d0b3144123f30693746b540b707621200
LeonelCamposM <leonelscamposm@gmail.com>
2023-12-03 22:21:41 -0600

    add scanning feature to rtos

--- AutonomusSumo/autonomusSumo.ino
+++ AutonomusSumo/autonomusSumo.ino
@@ -39,7 +39,7 @@ Command determineCommand(const SensorState& state) {
   if (state.frontLineDetected || state.backLineDetected) {
     cmd.action = 'S';  // 'S' para Stop
     cmd.speed = 255;
-  } else if (state.distance <= 40) {
+  } else if (state.distance < 80) {
     cmd.action = 'F';  // 'F' para Forward (hacia adelante)
     cmd.speed = 255;
   } else {
@@ -50,55 +50,91 @@ Command determineCommand(const SensorState& state) {
   return cmd;
 }
 
-void sensorTask(void* parameter) {
+Command evaluateSensorState() {
   SensorState currentState;
+  currentState.distance = getDistance();
+  //Serial.println("Distance:");
+  // Serial.println(currentState.distance);
+  currentState.frontLineDetected = isFrontLineDetected();
+  currentState.backLineDetected = isBackLineDetected();
+
+  Command currentCommand = determineCommand(currentState);
+  return currentCommand;
+}
+
+
+void sensorTask(void* parameter) {
   Command lastCommandSent = {};
   Command currentCommand;
 
   while (true) {
-    currentState.distance = getDistance();
-    currentState.frontLineDetected = isFrontLineDetected();
-    currentState.backLineDetected = isBackLineDetected();
-
-    currentCommand = determineCommand(currentState);
+    currentCommand = evaluateSensorState();  // Obtener el estado actual de los sensores
 
     if (currentCommand != lastCommandSent) {
-      xQueueSend(commandQueue, &currentCommand, portMAX_DELAY);
-      lastCommandSent = currentCommand;
+      if (lastCommandSent.action == 'S') {
+        // Escaneo de enemigos, se asume que el motor se mantiene moviendose a la derecha
+        int counter = 0;
+        Command newCommand;
+        while (counter < 40) {
+          newCommand = evaluateSensorState();  // Re-evaluar el estado de los sensores
+          if (newCommand.action == 'F' && newCommand.speed == 255) {
+            // Serial.println("SCANNING: encontrado enemigo");
+            break;
+          } else {
+            //Serial.println("SCANNING: No encontrado enemigo");
+          }
+          counter++;
+          // Serial.print("COUNTER: ");
+          // Serial.println(counter);
+          vTaskDelay(pdMS_TO_TICKS(100));
+        }
+        // Asegurarse de enviar el último comando evaluado después de salir del bucle
+        currentCommand = newCommand;
+        lastCommandSent = newCommand;
+        //Serial.print("COMMAND: enviando ");
+        Serial.println(newCommand.action);
+        xQueueSend(commandQueue, &newCommand, portMAX_DELAY);
+      } else {
+        // Serial.print("COMMAND: enviando ");
+        // Serial.println(currentCommand.action);
+        xQueueSend(commandQueue, &currentCommand, portMAX_DELAY);
+        lastCommandSent = currentCommand;
+      }
+    } else {
+      vTaskDelay(pdMS_TO_TICKS(100));
     }
-
-    vTaskDelay(pdMS_TO_TICKS(100));
   }
 }
 
 void motorTask(void* parameter) {
   Command cmd;
+  bool isMoving = false;
+
   while (true) {
     if (xQueueReceive(commandQueue, &cmd, portMAX_DELAY)) {
+      isMoving = true;
       switch (cmd.action) {
         case 'S':
           stop();
           moveBackward(cmd.speed);
-          delay(500);       
-          turnRight(cmd.speed); 
-          delay(500);       
-          break;
-        case 'B':
-          moveBackward(cmd.speed);
+          delay(500);
           break;
         case 'F':
           moveForward(cmd.speed);
-          break;
-        case 'L':
-          turnLeft(cmd.speed);
-          break;
-        case 'R':
-          turnRight(cmd.speed);
+          isMoving = false;  // Restablece el estado de movimiento
           break;
       }
     }
+
+    // Mantener el movimiento actual si no se ha recibido una nueva instrucción
+    // después de recibir un S
+    if (isMoving) {
+      turnRight(120);
+    }
   }
 }
+
+
 
 
 void loop() {
